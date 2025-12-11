de7251782237ad1aa7410bcdca32cedbd3cfb0e1
lucktu <67133690@qq.com>
2025-12-11 00:15:02 +0000

    add .yml

---
+++ trunk/configs/boards/KFRouter-S1/board.mk
@@ -0,0 +1,8 @@
+##################################################################
+# Board PID # Board Name       # PRODUCT # Note
+##################################################################
+# KFRouter-SOHO-S1 # meilinkr KFRouter-SOHO-S1  # MT7621  #
+##################################################################
+
+CFLAGS += -DBOARD_KFRouterSOHOS1 -DVENDOR_meilinkr
+BOARD_NUM_USB_PORTS=1
